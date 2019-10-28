66a3dff23dbd5376749536292dec27519d7a4f86
Wei Tang <hi@that.world>
2019-10-28 20:51:25 +0100

    Use parity's ethjson parsing crate for vmtests

--- jsontests/src/utils.rs
+++ jsontests/src/utils.rs
@@ -1,5 +1,13 @@
 use std::str::FromStr;
+use std::collections::BTreeMap;
 use primitive_types::{U256, H256, H160};
+use evm::backend::MemoryAccount;
+
+pub fn u256_to_h256(u: U256) -> H256 {
+	let mut h = H256::default();
+	u.to_big_endian(&mut h[..]);
+	h
+}
 
 pub fn unwrap_to_u256(s: &str) -> U256 {
 	if s.starts_with("0x") {
@@ -22,4 +30,40 @@ pub fn unwrap_to_h160(s: &str) -> H160 {
 pub fn unwrap_to_vec(s: &str) -> Vec<u8> {
 	assert!(s.starts_with("0x"));
 	hex::decode(&s[2..]).unwrap()
+}
+
+pub fn unwrap_to_account(s: &ethjson::spec::Account) -> MemoryAccount {
+	MemoryAccount {
+		balance: s.balance.clone().unwrap().into(),
+		nonce: s.nonce.clone().unwrap().into(),
+		code: s.code.clone().unwrap().into(),
+		storage: s.storage.as_ref().unwrap().iter().map(|(k, v)| {
+			(u256_to_h256(k.clone().into()), u256_to_h256(v.clone().into()))
+		}).collect(),
+	}
+}
+
+pub fn unwrap_to_state(a: &ethjson::spec::State) -> BTreeMap<H160, MemoryAccount> {
+	match &a.0 {
+		ethjson::spec::HashOrMap::Map(m) => {
+			m.iter().map(|(k, v)| {
+				(k.clone().into(), unwrap_to_account(v))
+			}).collect()
+		},
+		ethjson::spec::HashOrMap::Hash(_) => panic!("Hash can not be converted."),
+	}
+}
+
+pub fn assert_valid_state(a: &ethjson::spec::State, b: &BTreeMap<H160, MemoryAccount>) {
+	match &a.0 {
+		ethjson::spec::HashOrMap::Map(m) => {
+			assert_eq!(
+				&m.iter().map(|(k, v)| {
+					(k.clone().into(), unwrap_to_account(v))
+				}).collect::<BTreeMap<_, _>>(),
+				b
+			);
+		},
+		ethjson::spec::HashOrMap::Hash(h) => unimplemented!(),
+	}
 }
--- jsontests/src/vm.rs
+++ jsontests/src/vm.rs
@@ -1,128 +1,60 @@
 use std::collections::{HashMap, BTreeMap};
 use std::rc::Rc;
 use serde::{Serialize, Deserialize};
-use primitive_types::{H160, H256};
+use primitive_types::{H160, H256, U256};
 use evm::gasometer;
 use evm::backend::{Backend, ApplyBackend, MemoryBackend, MemoryVicinity, MemoryAccount};
 use evm::executor::StackExecutor;
+use ethjson::vm::Vm;
 use crate::utils::*;
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct Test {
-	pub env: Env,
-	pub exec: Exec,
-	pub pre: HashMap<String, Account>,
-	pub gas: Option<String>,
-	pub out: Option<String>,
-	pub post: Option<HashMap<String, Account>>,
-}
+#[derive(Deserialize, Debug)]
+pub struct Test(Vm);
 
 impl Test {
 	pub fn unwrap_to_pre_state(&self) -> BTreeMap<H160, MemoryAccount> {
-		self.pre.iter().map(|(k, v)| {
-			(unwrap_to_h160(&k), v.unwrap_to_account())
-		}).collect()
-	}
-
-	pub fn unwrap_to_post_state(&self) -> BTreeMap<H160, MemoryAccount> {
-		self.post.as_ref().unwrap().iter().map(|(k, v)| {
-			(unwrap_to_h160(&k), v.unwrap_to_account())
-		}).collect()
+		unwrap_to_state(&self.0.pre_state)
 	}
 
 	pub fn unwrap_to_vicinity(&self) -> MemoryVicinity {
 		MemoryVicinity {
-			gas_price: unwrap_to_u256(&self.exec.gas_price),
-			origin: unwrap_to_h160(&self.exec.origin),
+			gas_price: self.0.transaction.gas_price.clone().into(),
+			origin: self.0.transaction.origin.clone().into(),
 			block_hashes: Vec::new(),
-			block_number: unwrap_to_u256(&self.env.current_number),
-			block_coinbase: unwrap_to_h160(&self.env.current_coinbase),
-			block_timestamp: unwrap_to_u256(&self.env.current_timestamp),
-			block_difficulty: unwrap_to_u256(&self.env.current_difficulty),
-			block_gas_limit: unwrap_to_u256(&self.env.current_gas_limit),
+			block_number: self.0.env.number.clone().into(),
+			block_coinbase: self.0.env.author.clone().into(),
+			block_timestamp: self.0.env.timestamp.clone().into(),
+			block_difficulty: self.0.env.difficulty.clone().into(),
+			block_gas_limit: self.0.env.gas_limit.clone().into(),
 		}
 	}
 
 	pub fn unwrap_to_code(&self) -> Rc<Vec<u8>> {
-		Rc::new(unwrap_to_vec(&self.exec.code))
+		Rc::new(self.0.transaction.code.clone().into())
 	}
 
 	pub fn unwrap_to_data(&self) -> Rc<Vec<u8>> {
-		Rc::new(unwrap_to_vec(&self.exec.data))
+		Rc::new(self.0.transaction.data.clone().into())
 	}
 
 	pub fn unwrap_to_context(&self) -> evm::Context {
 		evm::Context {
-			address: unwrap_to_h160(&self.exec.address),
-			caller: unwrap_to_h160(&self.exec.caller),
-			apparent_value: unwrap_to_u256(&self.exec.value),
+			address: self.0.transaction.address.clone().into(),
+			caller: self.0.transaction.sender.clone().into(),
+			apparent_value: self.0.transaction.value.clone().into(),
 		}
 	}
 
 	pub fn unwrap_to_return_value(&self) -> Vec<u8> {
-		unwrap_to_vec(&self.out.as_ref().unwrap())
+		self.0.output.clone().unwrap().into()
 	}
 
 	pub fn unwrap_to_gas_limit(&self) -> usize {
-		unwrap_to_u256(&self.exec.gas).as_usize()
+		self.0.transaction.gas.clone().into()
 	}
 
 	pub fn unwrap_to_post_gas(&self) -> usize {
-		unwrap_to_u256(&self.gas.as_ref().unwrap()).as_usize()
-	}
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct Env {
-	pub current_coinbase: String,
-	pub current_difficulty: String,
-	pub current_gas_limit: String,
-	pub current_number: String,
-	pub current_timestamp: String,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct Exec {
-	pub address: String,
-	pub caller: String,
-	pub code: String,
-	pub data: String,
-	pub gas: String,
-	pub gas_price: String,
-	pub origin: String,
-	pub value: String,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct Account {
-	pub balance: String,
-	pub code: String,
-	pub nonce: String,
-	pub storage: HashMap<String, String>,
-}
-
-impl Account {
-	pub fn unwrap_to_account(&self) -> MemoryAccount {
-		MemoryAccount {
-			balance: unwrap_to_u256(&self.balance),
-			code: unwrap_to_vec(&self.code),
-			nonce: unwrap_to_u256(&self.nonce),
-			storage: self.storage.iter().map(|(k, v)| {
-				let ku = unwrap_to_u256(&k);
-				let mut k = H256::default();
-				ku.to_big_endian(&mut k[..]);
-
-				let vu = unwrap_to_u256(&v);
-				let mut v = H256::default();
-				vu.to_big_endian(&mut v[..]);
-
-				(k, v)
-			}).collect(),
-		}
+		self.0.gas_left.clone().unwrap().into()
 	}
 }
 
@@ -151,11 +83,11 @@ pub fn test(name: &str, test: Test) {
 	let (gas, values, logs) = executor.finalize();
 	backend.apply(values, logs);
 
-	if test.out.is_none() {
+	if test.0.output.is_none() {
 		print!("{:?} ", reason);
 
 		assert!(reason.is_error());
-		assert!(test.post.is_none() && test.gas.is_none());
+		assert!(test.0.post_state.is_none() && test.0.gas_left.is_none());
 
 		println!("succeed");
 	} else {
@@ -163,7 +95,7 @@ pub fn test(name: &str, test: Test) {
 		print!("{:?} ", reason);
 
 		assert_eq!(runtime.machine().return_value(), test.unwrap_to_return_value());
-		assert_eq!(backend.state(), &test.unwrap_to_post_state());
+		assert_valid_state(test.0.post_state.as_ref().unwrap(), &backend.state());
 		assert_eq!(gas, expected_post_gas);
 		println!("succeed");
 	}
